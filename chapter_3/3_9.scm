d70ecfd4870c67999b545ffc97cf6a1e5a7bd9a9
sci-42ver <wujsCM@tuta.io>
2024-08-30 16:25:46 +0800

    finish Exercise 3.9 test

--- chapter_3/3_9.scm
+++ chapter_3/3_9.scm
@@ -16,7 +16,7 @@
                    tan-derivative))
 (assign-handler!  tan     diff:tan    differential?)
 
-(define (test x func func-derivative)
+(define (test-unary x func func-derivative)
   ;; assume x=1.
   ;; 1+dx
   ;; See `replace-dx-differential` in extractor.scm and `make-infinitesimal`
@@ -27,12 +27,12 @@
         diff-term>?)))
 
   ;; func(1+dx)
-  (define (diff-func-1-terms)
+  (define diff-func-terms
     (diff-terms (func test-diff)))
-  (assert (= (func-derivative x)) (diff-coefficient (car (diff-func-1-terms))))
-  (assert (= (func x)) (diff-coefficient (cadr (diff-func-1-terms)))))
+  (assert (= (func-derivative x)) (diff-coefficient (car diff-func-terms)))
+  (assert (= (func x)) (diff-coefficient (cadr diff-func-terms))))
 
-(test 1 tan tan-derivative)
+(test-unary 1 tan tan-derivative)
 
 ;; IGNORE: to make get-implementation-value work.
 ; (environment-define system-global-environment 'atan1 atan1)
@@ -40,10 +40,11 @@
 ; (set! %arithmetic-operator-alist (cons '(atan1 (domain) domain) %arithmetic-operator-alist))
 ; (load "~/SICP_SDF/SDF_exercises/software/sdf/automatic-differentiation/utils.scm")
 
-;; test should be similar to diff:tan, so I skipped that.
+(define atan-derivative
+  (lambda (x) (/ 1 (+ 1 (expt x 2)))))
 (define diff:atan1
   (diff:unary-proc atan
-                   (lambda (x) (/ 1 (+ 1 (expt x 2))))))
+                   atan-derivative))
 ;; > atan1 is a function of one argument
 ;; > using atan1 for one argument and atan2 if given two arguments
 (assign-handler!  atan     diff:atan1    differential?)
@@ -67,16 +68,47 @@
   (lambda (x) (n:atan x)))
 (define atan2
   (lambda (y x) (n:atan (/ y x))))
+(define atan2-deriv-x
+  (lambda (x y) 
+    (* (/ 1 y) (/ 1 (+ 1 (expt (/ x y) 2))))))
+(define atan2-deriv-y
+  (lambda (x y)
+    (* (* -1 (/ x (square y))) (/ 1 (+ 1 (expt (/ x y) 2))))))
 (define diff:atan2
-  (diff:unary-proc atan
-                   (lambda (y x) (/ 1 (+ 1 (expt (/ y x) 2))))))
+  (diff:binary-proc atan
+                    atan2-deriv-x
+                    atan2-deriv-y
+                    ))
 
 ;; notice not use (list a b) since `preds` will be auto list.
 ;; > using atan1 for one argument and atan2 if given two arguments
 (assign-handler!  atan     atan2    number? number?)
 (assign-handler!  atan     atan1    number?)
 ;; > install an atan2 handler for differentials. Remember, it must coexist with the atan1 handler.
-(assign-handler!  atan     diff:atan2    differential? differential?)
+(assign-handler!  atan     diff:atan2    differential? any-object?)
+(assign-handler!  atan     diff:atan2    any-object? differential?)
 
 (assert (= (atan 1) (n:atan 1)))
 (assert (= (atan 1 1) (n:atan 1)))
+(test-unary 1 atan atan-derivative)
+
+(define (test-binary x y func func-derivative-x func-derivative-y)
+  (define test-diff-x
+    (make-differential 
+      (sort 
+        (list (make-diff-term x '()) (make-diff-term 1 (list (make-new-dx))))
+        diff-term>?)))
+  (define test-diff-y
+    (make-differential 
+      (sort 
+        (list (make-diff-term y '()) (make-diff-term 1 (list (make-new-dx))))
+        diff-term>?)))
+
+  ;; func(1+dx)
+  (define diff-func-terms
+    (diff-terms (func test-diff-x test-diff-y)))
+  (assert (= (func-derivative-x x y)) (diff-coefficient (car diff-func-terms)))
+  (assert (= (func-derivative-y x y)) (diff-coefficient (cadr diff-func-terms)))
+  (assert (= (func x y)) (diff-coefficient (caddr diff-func-terms)))
+  )
+(test-binary 1 1 atan atan2-deriv-x atan2-deriv-y)
